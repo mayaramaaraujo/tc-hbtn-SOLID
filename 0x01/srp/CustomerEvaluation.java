ad5ca7655dd1f133357f24f30dda4aa03b06281f
Mayara Araujo <63872581+mayaramaaraujo@users.noreply.github.com>
2022-04-06 07:35:01 -0300

    Add files via upload

--- 0x01/srp/CustomerEvaluation.java
+++ 0x01/srp/CustomerEvaluation.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
 public class CustomerEvaluation {
-
+	public void evaluationCustomer(){        
+    }
 }
--- 0x01/srp/CustomerService.java
+++ 0x01/srp/CustomerService.java
@@ -2,4 +2,18 @@ package com.example.demo;
 
 public class CustomerService {
 
+	public void ListCustomer(){
+    }
+
+    public void addCustomer(){
+    }
+
+    public void updateCustomer(){
+    }
+
+    public void deleteCustomer(){
+    }
+    
+    public void notifyCustomer(){
+    }
 }
