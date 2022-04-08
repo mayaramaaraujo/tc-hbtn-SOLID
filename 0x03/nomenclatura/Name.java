8cb87dcb6189155fbcbf4ba0ed49d744eaea71be
Mayara Araujo <63872581+mayaramaaraujo@users.noreply.github.com>
2022-04-08 07:35:34 -0300

    Add files via upload

--- 0x03/nomenclatura/Name.java
+++ 0x03/nomenclatura/Name.java
@@ -5,49 +5,31 @@ public class Name {
     //variáveis que falam por si só 
     //A
     public Date modifiedDate;
-    //public List<Testcase> findAllTestcasesByUser(Name.User user){};
-    //B
-    //public Date d; // modified date
-    public List<Testcase> find(Name.User user){}; //find all Testcases by User
+    public List<Testcase> findAllTestcasesByUser(Name.User user){};
 
     //Declarações corretas
     //A
     public List<Account> AccountList;
-    //B
-    //public Account[] AccountList; 
 
     // Sem Prefixos
-    //A
-    public void deleteUser(Name.User pUser){}; 
     //B
-    //public void deleteUser(Name.User user){};
+    public void deleteUser(Name.User user){};
 
     // Evite mapa mental    
-    //A
-    public URL r;
     //B
-    //public URL url;
+    public URL url;
 
     // Claase possui nomes próprios, funções devem ter verbos
     //A
-    //public class TableRepresenter {};
-    //public void saveCar(){};
-    //B
-    public class TableRepresent{};
-    public void car(){}; 
+    public class TableRepresenter {};
+    public void saveCar(){};
 
     // Uma palavra por conceito
     //A
-    //public saveUser(){}; saveAccount(){}; saveContract(){};
-    //B
-    public saveUser(){}; createAccount(){}; generateContract(){};
-
+    public saveUser(){}; saveAccount(){}; saveContract(){};
 
     //Use constants, enums instead of magic numbers, abbreviations
-    //A
-    public int daysInYear = 360;
-    public int hoursInWeek = 168;
     //B    
-    //public final int DAYS_IN_YEAR = 360;
-    //public final int HOURS_IN_WEEK = 168;
+    public final int DAYS_IN_YEAR = 360;
+    public final int HOURS_IN_WEEK = 168;
 }
