20d65fcc3c38598d9c6b318afb4adea84f798822
Mayara Araujo <63872581+mayaramaaraujo@users.noreply.github.com>
2022-04-08 07:32:46 -0300

    Update Name.java

--- 0x03/nomenclatura/Name.java
+++ 0x03/nomenclatura/Name.java
@@ -4,10 +4,10 @@ import java.util.List;
 public class Name {
     //variáveis que falam por si só 
     //A
-    //public Date modifiedDate;
+    public Date modifiedDate;
     //public List<Testcase> findAllTestcasesByUser(Name.User user){};
     //B
-    public Date d; // modified date
+    //public Date d; // modified date
     public List<Testcase> find(Name.User user){}; //find all Testcases by User
 
     //Declarações corretas
@@ -18,36 +18,36 @@ public class Name {
 
     // Sem Prefixos
     //A
-    //public void deleteUser(Name.User pUser){}; 
+    public void deleteUser(Name.User pUser){}; 
     //B
-    public void deleteUser(Name.User user){};
+    //public void deleteUser(Name.User user){};
 
     // Evite mapa mental    
     //A
-    //public URL r;
+    public URL r;
     //B
-    public URL url;
+    //public URL url;
 
     // Claase possui nomes próprios, funções devem ter verbos
     //A
-    public class TableRepresenter {};
-    public void saveCar(){};
+    //public class TableRepresenter {};
+    //public void saveCar(){};
     //B
-    //public class TableRepresent{};
-    //public void car(){}; 
+    public class TableRepresent{};
+    public void car(){}; 
 
     // Uma palavra por conceito
     //A
-    public saveUser(){}; saveAccount(){}; saveContract(){};
+    //public saveUser(){}; saveAccount(){}; saveContract(){};
     //B
-    //public saveUser(){}; createAccount(){}; generateContract(){};
+    public saveUser(){}; createAccount(){}; generateContract(){};
 
 
     //Use constants, enums instead of magic numbers, abbreviations
     //A
-    //public int daysInYear = 360;
-    //public int hoursInWeek = 168;
+    public int daysInYear = 360;
+    public int hoursInWeek = 168;
     //B    
-    public final int DAYS_IN_YEAR = 360;
-    public final int HOURS_IN_WEEK = 168;
+    //public final int DAYS_IN_YEAR = 360;
+    //public final int HOURS_IN_WEEK = 168;
 }
