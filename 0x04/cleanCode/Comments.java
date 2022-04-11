0facfef9b9d62ce46951c068534f7579ecea2f33
Mayara Araujo <63872581+mayaramaaraujo@users.noreply.github.com>
2022-04-11 07:52:37 -0300

    Add files via upload

--- 0x04/cleanCode/Comments.java
+++ 0x04/cleanCode/Comments.java
@@ -1,1 +1,30 @@
+public class Comments {
+    // Copyright (C) 2022 by CleanCode.
+    // Released under the terms of the GNU General Public License version 2 or later.
 
+    public Owner getTestcaseOwner(Testcase testcase){};
+
+    public Hours getHoursInQuarter(){};
+
+    // TODO Este métod deve ser removido
+    public void check(){
+        return;
+    }
+
+    // 3. Too much information
+    /*
+     * This class evaluates the overview of testcases which are cancelled from client
+     * the reasons of cancellation play a role in evaluation blablabla
+     */
+
+    /**
+     * <p> this method publishes results based on date
+     * </p>
+     * @param date date from when results should be published
+     * @return List of Result entities
+     * @since 16.0
+     */
+    public List<Result> publishResults(Date date) {
+        // some business logic here
+    }
+}
