f8c841878887d0a67b8b22fdc6f954a845f42a30
Mayara Araujo <63872581+mayaramaaraujo@users.noreply.github.com>
2022-04-11 08:01:58 -0300

    Create LoggerInFile.java

---
+++ 0x04/log/LoggerInFile.java
@@ -0,0 +1,33 @@
+package log\_file;
+
+import java.io.IOException;
+
+import java.util.logging.FileHandler;
+
+import java.util.logging.Logger;
+
+import java.util.logging.SimpleFormatter;
+
+public class LoggerInFile {
+
+    public static void main(String[] args) {
+
+        Logger logger = Logger.getLogger("MyLog");
+
+        FileHandler fileHandler;
+
+        fileHandler = new FileHandler("logs.txt");
+
+        Logger.AddHandler(fileHandler);
+
+        simpleFormatter simpleFormatter = new SimpleFormatter();
+
+        fileHandler.setFormatter(simpleFormatter);
+
+        Logger.info("Log test");
+
+        Logger.info("Hi In the main class test");
+
+    }
+
+}
