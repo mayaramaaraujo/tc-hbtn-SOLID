956dcbdd2eed917d62a0ad61bfb0f839335b4931
mayaramaaraujo <mayaramaaraujo98@gmail.com>
2022-04-12 08:45:06 -0300

    first

---
+++ 0x05/clean_arch/src/main/java/entities/User.java
@@ -0,0 +1,43 @@
+package entities;
+
+import java.util.Objects;
+
+public class User {
+    private Long id;
+    private String name;
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(name, user.name) && Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, password);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
